c53a2f96154f84fe8af6c308050b6b05683070c9
nope <nope>
2024-04-25 18:11:17 -0400

    init data

--- core/run.sh
+++ core/run.sh
@@ -1,3 +1,6 @@
 bash build.sh
+
+cargo install wasm-server-runner
+
 CARGO_TARGET_WASM32_UNKNOWN_UNKNOWN_RUNNER=wasm-server-runner \
     cargo run --target wasm32-unknown-unknown
--- core/src/main.rs
+++ core/src/main.rs
@@ -1,15 +1,14 @@
-use splr::*;
 mod console;
+mod lib;
 
-// fn main() {
-//     let v: Vec<Vec<i32>> = vec![vec![1, 2], vec![-1, 3], vec![1, -3], vec![-1, 2]];
-//     match Certificate::try_from(v) {
-//         Ok(Certificate::SAT(ans)) => println!("s SATISFIABLE: {:?}", ans),
-//         Ok(Certificate::UNSAT) => println!("s UNSATISFIABLE"),
-//         Err(e) => panic!("s UNKNOWN; {}", e),
-//     }
-// }
+use console::log;
+use splr::*;
 
 fn main() {
-    console::log!("Hello world!")
+    let v: Vec<Vec<i32>> = vec![vec![1, 2], vec![-1, 3], vec![1, -3], vec![-1, 2]];
+    match Certificate::try_from(v) {
+        Ok(Certificate::SAT(ans)) => log!("s SATISFIABLE: {:?}", ans),
+        Ok(Certificate::UNSAT) => log!("s UNSATISFIABLE"),
+        Err(e) => panic!("s UNKNOWN; {}", e),
+    }
 }
