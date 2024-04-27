c73a0b45f4d61ac2fdc15e2e0ef790490890058c
nope <nope>
2024-04-27 11:07:11 -0400

    add boolean distribution tests

--- core/src/lib/sat.rs
+++ core/src/lib/sat.rs
@@ -223,25 +223,47 @@ pub fn Implies(x1: BooleanExpression, x2: BooleanExpression) -> BooleanExpressio
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashSet;
+    use std::{ collections::HashSet, hash::{ Hash, Hasher } };
 
     use crate::lib::sat::*;
 
-    fn vec_vec_to_vec_hash(cnf: Vec<Vec<i32>>) -> Vec<HashSet<i32>> {
-        cnf.into_iter()
-            .map(|clause| {
+    // https://stackoverflow.com/questions/36562419/hashset-as-key-for-other-hashset
+    #[derive(Debug)]
+    struct HashedClause(HashSet<i32>);
+
+    impl PartialEq for HashedClause {
+        fn eq(&self, other: &HashedClause) -> bool {
+            self.0.is_subset(&other.0) && other.0.is_subset(&self.0)
+        }
+    }
+
+    impl Eq for HashedClause {}
+
+    impl Hash for HashedClause {
+        fn hash<H>(&self, state: &mut H) where H: Hasher {
+            let mut a: Vec<&i32> = self.0.iter().collect();
+            a.sort();
+            for s in a.iter() {
+                s.hash(state);
+            }
+        }
+    }
+
+    fn vec_vec_to_hash_hash(cnf: Vec<Vec<i32>>) -> HashSet<HashedClause> {
+        HashSet::from_iter(
+            cnf.into_iter().map(|clause| {
                 let n = clause.len();
-                let hashed = HashSet::from_iter(clause.into_iter());
+                let hashed = HashedClause(HashSet::from_iter(clause.into_iter()));
 
                 // No dupes should be removed, each variable should appear at most once
-                assert_eq!(hashed.len(), n);
+                assert_eq!(hashed.0.len(), n);
 
                 hashed
             })
-            .collect()
+        )
     }
 
-    fn assert_expression(expr: BooleanExpression, expected_value: Vec<Vec<i32>>) {
+    fn assert_cnf(expr: BooleanExpression, expected_value: Vec<Vec<i32>>) {
         let actual_value: Vec<Vec<i32>> = expr
             .dump()
             .iter()
@@ -249,8 +271,8 @@ mod tests {
             .collect();
 
         // Remove ordering
-        let expected_value: Vec<HashSet<i32>> = vec_vec_to_vec_hash(expected_value);
-        let actual_value: Vec<HashSet<i32>> = vec_vec_to_vec_hash(actual_value);
+        let expected_value = vec_vec_to_hash_hash(expected_value);
+        let actual_value = vec_vec_to_hash_hash(actual_value);
 
         assert_eq!(actual_value, expected_value);
     }
@@ -283,14 +305,14 @@ mod tests {
             literals: vec![l1, l2],
             expressions: vec![],
         };
-        assert_expression(expr, vec![vec![-1, 2]]);
+        assert_cnf(expr, vec![vec![-1, 2]]);
 
         let expr = BooleanExpression {
             mode: BooleanExpressionMode::And,
             literals: vec![l1, l2],
             expressions: vec![],
         };
-        assert_expression(expr, vec![vec![-1], vec![2]]);
+        assert_cnf(expr, vec![vec![-1], vec![2]]);
     }
 
     #[test]
@@ -311,6 +333,219 @@ mod tests {
                 expressions: vec![],
             }],
         };
-        assert_expression(or_or, vec![vec![1, 2, 3, 4]])
+        assert_cnf(or_or, vec![vec![1, 2, 3, 4]]);
+
+        //   (a + b + (c * d))
+        // = ((a + b + c) * (a + b + d))
+        let or_and = BooleanExpression {
+            mode: BooleanExpressionMode::Or,
+            literals: vec![l1, l2],
+            expressions: vec![BooleanExpression {
+                mode: BooleanExpressionMode::And,
+                literals: vec![l3, l4],
+                expressions: vec![],
+            }],
+        };
+        assert_cnf(or_and, vec![vec![1, 2, 3], vec![1, 2, 4]]);
+
+        // a * b * (c + d)
+        let and_or = BooleanExpression {
+            mode: BooleanExpressionMode::And,
+            literals: vec![l1, l2],
+            expressions: vec![BooleanExpression {
+                mode: BooleanExpressionMode::Or,
+                literals: vec![l3, l4],
+                expressions: vec![],
+            }],
+        };
+        assert_cnf(and_or, vec![vec![1], vec![2], vec![3, 4]]);
+
+        //   a * b * (c * d)
+        // = a * b * c * d
+        let and_or = BooleanExpression {
+            mode: BooleanExpressionMode::And,
+            literals: vec![l1, l2],
+            expressions: vec![BooleanExpression {
+                mode: BooleanExpressionMode::And,
+                literals: vec![l3, l4],
+                expressions: vec![],
+            }],
+        };
+        assert_cnf(and_or, vec![vec![1], vec![2], vec![3], vec![4]])
+    }
+
+    #[test]
+    fn expr_or_or_or() {
+        let l1 = Literal { variable: 1, negate: false };
+        let l2 = Literal { variable: 2, negate: false };
+        let l3 = Literal { variable: 3, negate: false };
+        let l4 = Literal { variable: 4, negate: false };
+        let l5 = Literal { variable: 5, negate: false };
+
+        //   (a + (b + c) + (d + e))
+        // = (a + b + c + d + e)
+        let expr = BooleanExpression {
+            mode: BooleanExpressionMode::Or,
+            literals: vec![l1],
+            expressions: vec![
+                BooleanExpression {
+                    mode: BooleanExpressionMode::Or,
+                    literals: vec![l2, l3],
+                    expressions: vec![],
+                },
+                BooleanExpression {
+                    mode: BooleanExpressionMode::Or,
+                    literals: vec![l4, l5],
+                    expressions: vec![],
+                }
+            ],
+        };
+        assert_cnf(expr, vec![vec![1, 2, 3, 4, 5]]);
+    }
+
+    #[test]
+    fn expr_or_or_and() {
+        let l1 = Literal { variable: 1, negate: false };
+        let l2 = Literal { variable: 2, negate: false };
+        let l3 = Literal { variable: 3, negate: false };
+        let l4 = Literal { variable: 4, negate: false };
+        let l5 = Literal { variable: 5, negate: false };
+
+        //   (a + (b + c) + (d * e))
+        // = (a + b + c + d) * (a + b + c + e)
+        let expr = BooleanExpression {
+            mode: BooleanExpressionMode::Or,
+            literals: vec![l1],
+            expressions: vec![
+                BooleanExpression {
+                    mode: BooleanExpressionMode::Or,
+                    literals: vec![l2, l3],
+                    expressions: vec![],
+                },
+                BooleanExpression {
+                    mode: BooleanExpressionMode::And,
+                    literals: vec![l4, l5],
+                    expressions: vec![],
+                }
+            ],
+        };
+        assert_cnf(expr, vec![vec![1, 2, 3, 4], vec![1, 2, 3, 5]]);
+    }
+
+    #[test]
+    fn expr_or_and_and() {
+        let l1 = Literal { variable: 1, negate: false };
+        let l2 = Literal { variable: 2, negate: false };
+        let l3 = Literal { variable: 3, negate: false };
+        let l4 = Literal { variable: 4, negate: false };
+        let l5 = Literal { variable: 5, negate: false };
+
+        //   a + (b * c) + (d * e)
+        // = (a + b) * (a + c) + (d * e)
+        // = (d + (a + b) * (a + c)) * (e + (a + b) * (a + c))
+        // = (dab) * (dac) * (eab) * (eac)
+        let expr = BooleanExpression {
+            mode: BooleanExpressionMode::Or,
+            literals: vec![l1],
+            expressions: vec![
+                BooleanExpression {
+                    mode: BooleanExpressionMode::And,
+                    literals: vec![l2, l3],
+                    expressions: vec![],
+                },
+                BooleanExpression {
+                    mode: BooleanExpressionMode::And,
+                    literals: vec![l4, l5],
+                    expressions: vec![],
+                }
+            ],
+        };
+        assert_cnf(expr, vec![vec![4, 1, 2], vec![4, 1, 3], vec![5, 1, 2], vec![5, 1, 3]]);
+    }
+
+    #[test]
+    fn expr_and_or_or() {
+        let l1 = Literal { variable: 1, negate: false };
+        let l2 = Literal { variable: 2, negate: false };
+        let l3 = Literal { variable: 3, negate: false };
+        let l4 = Literal { variable: 4, negate: false };
+        let l5 = Literal { variable: 5, negate: false };
+
+        // a * (b + c) * (d + e)
+        let expr = BooleanExpression {
+            mode: BooleanExpressionMode::And,
+            literals: vec![l1],
+            expressions: vec![
+                BooleanExpression {
+                    mode: BooleanExpressionMode::Or,
+                    literals: vec![l2, l3],
+                    expressions: vec![],
+                },
+                BooleanExpression {
+                    mode: BooleanExpressionMode::Or,
+                    literals: vec![l4, l5],
+                    expressions: vec![],
+                }
+            ],
+        };
+
+        assert_cnf(expr, vec![vec![1], vec![2, 3], vec![4, 5]]);
+    }
+
+    #[test]
+    fn expr_and_or_and() {
+        let l1 = Literal { variable: 1, negate: false };
+        let l2 = Literal { variable: 2, negate: false };
+        let l3 = Literal { variable: 3, negate: false };
+        let l4 = Literal { variable: 4, negate: false };
+        let l5 = Literal { variable: 5, negate: false };
+
+        // a * (b + c) * (d * e)
+        let expr = BooleanExpression {
+            mode: BooleanExpressionMode::And,
+            literals: vec![l1],
+            expressions: vec![
+                BooleanExpression {
+                    mode: BooleanExpressionMode::Or,
+                    literals: vec![l2, l3],
+                    expressions: vec![],
+                },
+                BooleanExpression {
+                    mode: BooleanExpressionMode::And,
+                    literals: vec![l4, l5],
+                    expressions: vec![],
+                }
+            ],
+        };
+
+        assert_cnf(expr, vec![vec![1], vec![2, 3], vec![4], vec![5]]);
+    }
+    #[test]
+    fn expr_and_and_and() {
+        let l1 = Literal { variable: 1, negate: false };
+        let l2 = Literal { variable: 2, negate: false };
+        let l3 = Literal { variable: 3, negate: false };
+        let l4 = Literal { variable: 4, negate: false };
+        let l5 = Literal { variable: 5, negate: false };
+
+        // a * (b * c) * (d * e)
+        let expr = BooleanExpression {
+            mode: BooleanExpressionMode::And,
+            literals: vec![l1],
+            expressions: vec![
+                BooleanExpression {
+                    mode: BooleanExpressionMode::And,
+                    literals: vec![l2, l3],
+                    expressions: vec![],
+                },
+                BooleanExpression {
+                    mode: BooleanExpressionMode::And,
+                    literals: vec![l4, l5],
+                    expressions: vec![],
+                }
+            ],
+        };
+
+        assert_cnf(expr, vec![vec![1], vec![2], vec![3], vec![4], vec![5]]);
     }
 }
